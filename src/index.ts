5e6ebed41a6b91f61f058ea4d1fcd5421efea9c0
Jimin Park <pjm950@gmail.com>
2025-10-10 02:08:21 +0900

    Export math.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from "./data-structure/index.js";
 export * from "./function/index.js";
 export * from "./iterator/index.js";
+export * from "./math/index.js";
 export * from "./string/index.js";
 export type * from "./type/index.ts";
