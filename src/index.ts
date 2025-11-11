242d37ad55a0698dc987e74563cb8d2ec6dc0136
Jimin Park <pjm950@gmail.com>
2025-11-11 10:52:01 +0900

    Added `assertEqualType`.

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,4 @@ export * from "./function/index.js";
 export * from "./iterator/index.js";
 export * from "./math/index.js";
 export * from "./string/index.js";
-export type * from "./type/index.ts";
+export * from "./type/index.js";
--- src/type/index.ts
+++ src/type/index.ts
@@ -3,3 +3,7 @@ export type Nullable<T> = T | null | undefined;
 
 /** Null-like type, that can be checked via `== null`. */
 export type NullLike = null | undefined;
+
+/** Asserts that two types are equal. */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function assertEqualType<T, U extends T>(..._: [T] extends [U] ? [] : [never]): void {}
