6d65edfdeaa02da9f0c5ec8673b7fc210439eda5
Jimin Park <pjm950@gmail.com>
2025-11-11 11:03:20 +0900

    Added arrayAtOrExtend functions.

--- src/data-structure/array.ts
+++ src/data-structure/array.ts
@@ -16,3 +16,41 @@ export function partition<T>(arr: T[], predicate: (value: T, index: number, arra
 
     return [falsey, truthy];
 }
+
+/**
+ * Gets the element at the given index.
+ * If the index is out of bounds, the array will be extended with shallow copies of `default_value`.
+ * 
+ * @param arr
+ * @param index
+ * @param default_value
+ */
+export function arrayAtOrExtend<T extends NonNullable<object>>(arr: T[], index: number, default_value: T): T {
+    if(index < 0) throw new RangeError("Index must be non-negative");
+    if(index < arr.length) return arr[index];
+
+    for(let i = arr.length; i <= index; ++i) {
+        arr.push({...default_value});
+    }
+
+    return arr[index];
+}
+
+/**
+ * Gets the element at the given index.
+ * If the index is out of bounds, the array will be extended with the result of `f(index)`.
+ * 
+ * @param arr 
+ * @param index 
+ * @param f 
+ */
+export function arrayAtOrExtendWith<T extends NonNullable<object>>(arr: T[], index: number, f: (index: number) => T): T {
+    if(index < 0) throw new RangeError("Index must be non-negative");
+    if(index < arr.length) return arr[index];
+
+    for(let i = arr.length; i <= index; ++i) {
+        arr.push(f(i));
+    }
+
+    return arr[index];
+}
