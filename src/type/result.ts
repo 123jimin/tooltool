f19f02b2fab43886e5110d7221a03b446d1ddfde
Jimin Park <pjm950@gmail.com>
2025-11-19 19:05:50 +0900

    Added aux functions for result.

--- src/type/result.ts
+++ src/type/result.ts
@@ -10,6 +10,14 @@ export interface Err<E = Error> {
 
 export type Result<T, E = Error> = Ok<T> | Err<E>;
 
+export function isResultOk<T, E = Error>(result: Result<T, E>): result is Ok<T> {
+    return result.ok;
+}
+
+export function isResultErr<T, E = Error>(result: Result<T, E>): result is Err<E> {
+    return !result.ok;
+}
+
 export function resultUnwrap<T, E = Error>(result: Result<T, E>): T {
     if (result.ok) {
         return result.value;
